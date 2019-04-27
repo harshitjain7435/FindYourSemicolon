2bd12ad3d04927fe556ebcb32fd4116e748a11db
harshitjain7435 <harshitjain7435@gmail.com>
2019-04-28 00:51:43 +0530

    Add files via upload

---
+++ Problem1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    char ch;
+    int n;
+    do
+    {
+        printf("Enter your Number:");
+        scanf("%d",&n);
+        if(n%2)
+            printf("Entered number is odd");
+        else
+            printf("Entered number is even");
+        fflush(stdin);
+        printf("\nDo you want to enter one more number(y/n):");
+        scanf("%c",&ch);
+    }while(toupper(ch)=='Y');
+}
